8d64bff59b20814eed281d46e7a2e3a7cdce6f99
asherkhb <asherkhb@gmail.com>
2020-10-22 22:55:38 -0600

    Use Flask RESTX

--- server/research-environment-server/api.py
+++ server/research-environment-server/api.py
@@ -1,46 +1,54 @@
 from flask import Flask, jsonify, request
+from flask_restx import Resource, Api
+
 from applications import *
 from datasets import *
 
 app = Flask(__name__)
+api = Api(app)
 
 
-@app.route('/status')
-def hello_world():
-    return jsonify('OK')
+@api.route('/status')
+class Status(Resource):
+    def get(self):
+        return jsonify('OK')
 
 
-@app.route('/applications', methods=['GET', 'POST'])
-def applications():
-    if request.method == 'POST':
-        return launch_application()
-    else:
+@api.route('/applications')
+class Applications(Resource):
+    def get(self):
         return list_applications()
+    def post(self):
+        return launch_application()
 
 
-@app.route('/applications/<application_id>', methods=['GET', 'POST', 'DELETE'])
-def applications_by_id(application_id):
-    if request.method == 'POST':
-        return update_application(application_id)
-    elif request.method == 'DELETE':
-        return delete_application(application_id)
-    else:
+@api.route('/applications/<application_id>')
+class ApplicationsById(Resource):
+    def get(self, application_id):
         return describe_application(application_id)
+    def post(self, application_id):
+        return update_application(application_id)
+    def delete(self, application_id):
+        return delete_application(application_id)
 
 
-@app.route('/datasets', methods=['GET', 'POST'])
-def datasets():
-    if request.method == 'POST':
-        return create_dataset()
-    else:
+@api.route('/datasets')
+class Datasets(Resource):
+    def get(self):
         return list_datasets()
+    def post(self):
+        return create_dataset()
 
 
-@app.route('/datasets/<dataset_id>', methods=['GET', 'POST', 'DELETE'])
-def datasets_by_id(dataset_id):
-    if request.method == 'POST':
-        return update_dataset(dataset_id)
-    elif request.method == 'DELETE':
-        return delete_dataset(dataset_id)
-    else:
+@api.route('/datasets/<dataset_id>')
+class DatasetsById(Resource):
+    def get(self, dataset_id):
         return describe_dataset(dataset_id)
+    def post(self, dataset_id):
+        return update_dataset(dataset_id)
+    def delete(self, dataset_id):
+        return delete_dataset(dataset_id)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
